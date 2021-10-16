e880c9d1d76df51386c995a1242716f1613090e3
Mahmudul Hasan <shuvonovo@gmail.com>
2021-10-16 14:18:10 +0600

    add

--- src/components/introduction/Introduction.component.jsx
+++ src/components/introduction/Introduction.component.jsx
@@ -1,7 +1,39 @@
+import './Introduction.styles.css';
+// import Button from 'react-bootstrap/Button'
+// import { Button } from 'react-bootstrap'
+import Hello from '../images/hello.jpeg'
 const Introduction = () => {
     return (
-        <div>
-            Mahmudul Hasan
+        <div className="intro">
+
+            <div className="intro-left">
+                <div className="intro-left-wrapper">
+                    <h3 className="intro-start">Hello,My name is </h3>
+                    <h2 className="intro-name"><strong>Mahmudul Hasan</strong></h2>
+
+                    <div className="intro-title">
+                        <div className="intro-title-wrapper">
+
+                            <div className="intro-title-item">Web developer</div>
+                            <div className="intro-title-item">Computer Science Graduate</div>
+                            <div className="intro-title-item">Basketball Player</div>
+                        </div>
+                    </div>
+
+
+
+                    <div className="des">
+                        I like to solve problems.I am Mern Stack Web Developer.Currently I working on React.js
+                    </div>
+
+                </div>
+            </div>
+            <div className="intro-right">
+                <img src={Hello} alt='' className="image" />
+            </div>
+
+
+            {/* <div className="intro-right"><Button>Click</Button></div> */}
         </div>
     )
 }
